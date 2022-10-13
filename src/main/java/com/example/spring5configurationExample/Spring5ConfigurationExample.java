26102444528d6c409429835c4b8365efa718aa51
Chowdhury Mohammad Irfanuddin <irfanuddincmd@gmail.com>
2022-10-13 13:30:42 +0600

    XML Based Configuration

--- src/main/java/com/example/spring5configurationExample/Spring5ConfigurationExample.java
+++ src/main/java/com/example/spring5configurationExample/Spring5ConfigurationExample.java
@@ -2,8 +2,10 @@ package com.example.spring5configurationExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:joke-configuration.xml")
 public class Spring5ConfigurationExample {
 
     public static void main(String[] args) {
